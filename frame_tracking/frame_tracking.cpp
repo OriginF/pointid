223bab7503ad9ce09d5e44d5ef6cf8bae63c3ecb
fqy22 <fengqiyuan18@163.com>
2022-09-26 13:36:52 +0800

    fix size

--- frame_tracking/frame_tracking.cpp
+++ frame_tracking/frame_tracking.cpp
@@ -7,7 +7,7 @@ void frame_tracking_run(){
     // writer.open("./ans.mp4",CV_FOURCC('M','P','4','2'),30,Size(SP.row,SP.col),true);
     while(!convert_done);
 
-    writer.open("./ans01.avi",VideoWriter::fourcc('M','J','P','G'),25,Size(1000,600),true);
+    writer.open("./ans01.avi",VideoWriter::fourcc('M','J','P','G'),25,Size(SP.col,SP.row),true);
     // cout << "init: "<< SP.row << " " << SP.col << endl;
     if(!writer.isOpened()){cout << "video open error!" << endl;exit(-1);};
     while(!read_done){
@@ -72,7 +72,7 @@ int frame_tracking(VideoWriter writer){
     // }
 
     //最后绘制一下所有的点簇的点，用来比较
-    drawing=Mat(600,1000,CV_8UC3,Scalar(255,255,255));
+    drawing=Mat(SP.row,SP.col,CV_8UC3,Scalar(255,255,255));
     for(int i=0;i<g_side;i++){
         for(int j=0;j<g_side;j++){
             circle(drawing,tracked_location[i][j],3,Scalar(0,0,0),1,LINE_8,0);
